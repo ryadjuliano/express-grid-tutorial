d0e8b102b40b6ece022e2ec61d569f229fc2aa9d
ryadjuliano <ryadjuliano@gmail.com>
2019-11-27 08:53:28 +0700

    EXPRESS TEST

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var path = __dirname + '/views/';
+// const axios = require('axios');
+// const fs = require('fs');
 
-const axios = require('axios');
-const fs = require('fs');
 
+// var config = {
+//   responseType: 'stream'
+// };
 
-var config = {
-  responseType: 'stream'
-};
-
-let url = 'https://images.dog.ceo/breeds/setter-english/n02100735_4870.jpg';
+// let url = 'https://images.dog.ceo/breeds/setter-english/n02100735_4870.jpg';
 /* home page. */
-router.get('/', function(req, res, next) {
+// router.get('/', function(req, res, next) {
   
-  // res.render('index', { title: 'Hey', message: 'Hello there!' })
-  async function getImage() {
+//   // res.render('sindex')
+//   res.render(
+//     'index', 
+//     { 
+//         message: 'Lorem ipsum sit dolor amet', 
+//         user: {name:'suyono', email:'suyono@example.com', website: 'http://www.suyono.com'} 
+//     }
+//   );
+// });
 
-    let resp = await axios.get(url, config);
-    resp.data.pipe(fs.createWriteStream('image.jpg'));
-}
-getImage();
+router.get('/',function(req, res){
+  res.sendFile(path + 'index.html')
+});
+  
+
+//   async function getImage() {
+
+//     let resp = await axios.get(url, config);
+//     resp.data.pipe(fs.createWriteStream('image.jpg'));
+// }
+// getImage();
 
   // res.render(
   //   'index', 
@@ -30,7 +44,7 @@ getImage();
   // );
 
 
-});
+
 
 
 
